2f4414a5460cf6126cab9a2b1c336ac10050c4f5
Vinay Kumar Pandey <147065919+VinayPandey557@users.noreply.github.com>
2024-09-04 23:16:30 +0530

    Add files via upload

---
+++ db.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://vinaypandey46158:pandey557@cluster0.vceuh.mongodb.net/paytm');
+
+
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    firstName: String,
+    lastName: String
+    
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+})
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = {
+    User,
+    Account
+}
